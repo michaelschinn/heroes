6a4736e1a7c7d49b2302b4cbeaca41a77d1b0ee7
michael.stephen.chinn <michael.stephen.chinn@gmail.com>
2021-11-05 09:56:31 -0400

    Finished CRUD functions

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-#session_start();
+session_start();
 
 #echo "<h1>Heroes</h1>";
 
@@ -12,16 +12,17 @@ $dbName = "superheroes";
 
 
 $heroObj = [];
+$conn = null;
 
 function connectDb()
 {
-    global $servername, $username, $password, $dbName;
+    global $servername, $username, $password, $dbName, $conn;
     $conn = new mysqli($servername, $username, $password, $dbName);
 
     if ($conn->connect_error) {
         die("Connection failed." . $conn->connect_error);
     }
-
+    $_SESSION["connection"] = $conn;
     return $conn;
 }
 
@@ -33,7 +34,7 @@ function queryDb($conn, $sql, $successMessage)
         print_r("ERROR: " . $sql . "\n" . $conn->error);
     }
 }
-
+/*
 function queryDbInsert($conn, $sql, $successMessage)
 {
     $result = $conn->query($sql);
@@ -49,21 +50,12 @@ function queryDbInsert($conn, $sql, $successMessage)
         print_r("ERROR: " . $sql . "\n" . $conn->error);
     }
 }
-
+*/
 function queryDbSelect($conn, $sql)
 {
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            print_r(
-                "ID: " . $row["id"] . "\n" .
-                    "Name: " . $row["name"] . "\n" .
-                    "About Me: " . $row["about_me"] . "\n" .
-                    "Biography: " . $row["biography"] . "\n" .
-                    "Image: " . $row["image_url"] . "\n\n"
-            );
-        }
-        #print_r($result);
+        return $result;
     } else {
         print_r("0 results.");
     }
@@ -71,7 +63,6 @@ function queryDbSelect($conn, $sql)
 
 function createHero()
 {
-    global $heroObj;
     $errors = "";
 
     if (!isset($_GET["name"])) {
@@ -83,9 +74,6 @@ function createHero()
     if (!isset($_GET["biography"])) {
         $errors .= "biography ";
     }
-    if (!isset($_GET["ability"])) {
-        $errors .= "ability ";
-    }
     if (strlen($errors) > 0) {
         print_r("ERROR 422: Missing " . $errors . ".");
         return;
@@ -94,22 +82,56 @@ function createHero()
     $name = $_GET["name"];
     $about_me = $_GET["about_me"];
     $biography = $_GET["biography"];
-    $ability = $_GET["ability"];
     $image = isset($_GET["image"]) ? $_GET["image"] : null;
+    $ability = isset($_GET["ability"]) ? $_GET["ability"] : "";
 
     $sql = "INSERT INTO heroes (name, about_me, biography, image_url)
     VALUES ('$name', '$about_me', '$biography', '$image')";
 
-    queryDbInsert(connectDb(), $sql, "Hero successfully created.");
-    
-    if (isset($heroObj)) {
-        $lastId = $heroObj["id"];
-        $sql = "INSERT INTO abilities (hero_id, ability_id) VALUES ($lastId, $ability)";
-        queryDb(connectDb(), $sql, "Ability successfully added.");
+    queryDb(connectDb(), $sql, "Hero successfully created.");
+
+    if (isset($_SESSION["connection"])) {
+        $heroId = $_SESSION["connection"]->insert_id;
+        if (strlen($ability) > 0) {
+            $abilities = strlen($ability) === 1 ? str_split($ability) : explode(", ", $ability);
+            foreach ($abilities as $key => $value) {
+                if ($key === 0) {
+                    $sql = "INSERT INTO abilities (hero_id, ability_id)
+                    VALUES ('$heroId', '$value');";
+                } else {
+                    $sql .= "INSERT INTO abilities (hero_id, ability_id)
+                    VALUES ('$heroId', '$value');";
+                }
+            }
+            queryDb(connectDb(), $sql, "Abilities added.");
+        }
+    }
+}
+/*
+function addAbility()
+{
+    $ability = isset($_GET["ability"]) ? $_GET["ability"] : null;
+    $errors = "";
+
+    if (!isset($_GET["id"])) {
+        $errors .= "id";
+    }
+    if (!isset($_GET["ability"])) {
+        $errors .= "ability";
     }
 
-}
+    $id = $_GET["id"];
+    $ability = $_GET["ability"];
 
+    $sql = "INSERT INTO abilities (hero_id, ability_id) VALUES ($id, $ability)";
+    queryDb(connectDb(), $sql, "Ability successfully added to " . $id . ".");
+
+    if (strlen($errors) > 0) {
+        print_r("ERROR 422: Missing " . $errors . ".");
+        return;
+    }
+}
+*/
 function readHero()
 {
 
@@ -129,13 +151,37 @@ function readHero()
     # TODO: error check in case hero does not match id or all.
     # TODO: check if id is within bounds.
 
-    queryDbSelect(connectDb(), $sql);
+    $allHeros = queryDbSelect(connectDb(), $sql);
+
+    while ($row = $allHeros->fetch_assoc()) {
+        $heroId = $row["id"];
+        $output = "\nID: " . $row["id"] . "\n" .
+            "Name: " . $row["name"] . "\n" .
+            "About Me: " . $row["about_me"] . "\n" .
+            "Biography: " . $row["biography"] . "\n" .
+            "Image: " . $row["image_url"] . "\n";
+
+        $sql = "SELECT ability_id FROM abilities WHERE hero_id = '$heroId'";
+
+        $abilities = queryDbSelect(connectDb(), $sql);
+        while($row = $abilities->fetch_assoc()){
+            $abilityId = $row["ability_id"];
+            $sql = "SELECT ability FROM ability_type WHERE id = '$abilityId'";
+            $ability = queryDbSelect(connectDb(), $sql);
+            while($row = $ability->fetch_assoc()){
+                $abilityName = $row["ability"];
+                $output .= "Ability: '$abilityName' \n";
+            }
+        }
+        print_r($output);
+    }
 }
 
 function updateHero()
 {
     print_r("Update Hero Fired!");
     $updates = "";
+    $abilities = "";
     try {
         // check to see if the index in the get array exists
         // if it does modify the query
@@ -154,6 +200,9 @@ function updateHero()
         if (isset($_GET["image_url"])) {
             $updates .= "image_url = '" . $_GET['image_url'] . "', ";
         }
+        /*if (isset($_GET["ability"])) {
+            $abilities .= "ability_id = '" . $_GET['ability'] . "', ";
+        }*/
         $updates = rtrim($updates, ", ");
         if (!isset($index)) {
             print_r("ERROR 422: Index is required.");
@@ -163,6 +212,8 @@ function updateHero()
             $sql = "UPDATE heroes SET $updates WHERE id=$index";
             var_dump($sql);
             queryDb(connectDb(), $sql, "Hero successfully updated.");
+
+            /*$sql = "UPDATE abilities SET $abilities WHERE hero_id=$index"*/
         }
     } catch (Exception $e) {
         echo 'Caught exception: ',  $e->getMessage(), "\n";
